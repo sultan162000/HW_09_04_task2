6db49c5cafa4553f80b949cec15306b062547834
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-10 22:39:05 +0500

    Добален интерфейсы IPlayable и IRecodable и класс Player

--- Program.cs
+++ Program.cs
@@ -9,4 +9,37 @@ namespace HW_09_04_task2
             Console.WriteLine("Hello World!");
         }
     }
+    interface IPlayable{
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable{
+        void Record();
+        void Pause();
+        void Stop();
+
+    }
+
+    class Player : IPlayable, IRecodable{
+        public void Play(){
+            System.Console.WriteLine("Запуск музыки...");
+        }
+        void IPlayable.Pause(){
+            System.Console.WriteLine("Пауза музыки");
+        }
+        void IPlayable.Stop(){
+            System.Console.WriteLine("Стоп музыки");
+        }
+        public void Record(){
+            System.Console.WriteLine("Запись звука");
+        }
+        void IRecodable.Pause(){
+            System.Console.WriteLine("Пауза записа");
+        }
+        void IRecodable.Stop(){
+            System.Console.WriteLine("Стоп записа");
+        }
+
+    }
 }
